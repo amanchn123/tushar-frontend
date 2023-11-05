6a61725f5910367927dc5101769244a665f65a49
amanchn123 <118670647+amanchn123@users.noreply.github.com>
2023-11-05 13:51:43 +0530

    e paper pop

--- src/components/Layout/Header/HeaderFour.js
+++ src/components/Layout/Header/HeaderFour.js
@@ -1,12 +1,44 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import LogoWithSearch from "./LogoWithSearch";
 import NavigationBar from "./NavigationBar";
+import { Box, Modal } from "@mui/material";
+import Link from "next/link";
 // import TopbarThree from './TopbarThree';
 // import { useMediaQuery } from '@mui/material';
+import { Domain } from "@/components/api/domain";
+import CloseIcon from '@mui/icons-material/Close';
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 300,
+  bgcolor: "#fbe1c3",
+  border: "2px solid orange",
+  boxShadow: 24,
+  borderRadius: "2%",
+  p: 4,
+};
 
 export default function HeaderFour({ action }) {
   // const portJoin = useMediaQuery("(max-width:990px)");
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    const alertTimeout = setTimeout(() => {
+      setOpen(true);
+    }, 12000); // 10 seconds in milliseconds
+
+    return () => {
+      // Clear the timeout when the component unmounts to avoid memory leaks
+      clearTimeout(alertTimeout);
+    };
+  }, []);
+
   return (
     <header className="header-area header-style-2 header-style-4">
       {/* {portJoin?"":<TopbarThree />} */}
@@ -16,7 +48,7 @@ export default function HeaderFour({ action }) {
         <div className="container custom-container ">
           <div className="menubar-b ">
             <div className="row align-items-center position-relative shadow-md mt-2 ">
-              <div className="col-lg-10 col-sm-3 col-3 ">
+              <div className="col-lg-10 ocl-sm-3 col-3 ">
                 <div className="header-menu newsprk-header-main-menu newsprk-header-main-menu-dark header-menu-style-2 ">
                   <div
                     onClick={(e) => action(e)}
@@ -34,6 +66,34 @@ export default function HeaderFour({ action }) {
                   </div>
                 </div>
               </div>
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                <div className="-mt-5 flex justify-end -mr-4 cursor-pointer"><CloseIcon onClick={()=>setOpen(false)} /></div>
+                Click Here to Read Todays <Link
+                href={`${Domain}/E-News`}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  fontSize: "20px",
+                  // marginRight: "30px",
+                  // color: "white",
+                }}
+              >
+              
+                <i
+                  style={{ fontSize: "30px" }}
+                  class="fas fa-newspaper  mr-2"
+                ></i>
+                <span className="flex items-center text-center">
+                  E-<p className="flex text-center mt-3"> Paper</p>
+                </span>{" "}
+              </Link></Box>
+              </Modal>
             </div>
           </div>
         </div>
